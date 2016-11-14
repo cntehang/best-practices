57dcf2f5db51231741f47039a6e1ec70f2c0ae64
Ying Liu <yingliu4203@gmail.com>
2016-11-14 09:07:51 +0800

    delete vue

--- vue/vue_ctp_sample/src/vuex/modules/products/actions.js
+++
@@ -1,7 +0,0 @@
-import { http } from 'vue'
-import { FETCH_PRODUCTS } from './mutation-types'
-
-export function fetchProducts ({ commit }) {
-  return http.get('products/')
-    .then((response) => commit(FETCH_PRODUCTS, response.body.results))
-}
--- vue/vue_ctp_sample/src/vuex/modules/products/mutations.js
+++
@@ -1,8 +0,0 @@
-import { FETCH_PRODUCTS } from './mutation-types'
-
-// special syntax due to the computed property name
-export const mutations = {
-  [FETCH_PRODUCTS] (state, products) {
-    state.products = products
-  }
-}
